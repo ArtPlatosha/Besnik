a91f4391915393e0e635c7ca46f0d068fd000b05
ArtPlatosha <zipex98@gmail.com>
2023-05-15 10:24:11 +0300

    commit: 6th commit
    activating mob. nav (JS)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // activate the hamb menu and mobile navigation
 const hamb = document.querySelector('.hamb');
+const nav = document.querySelector('.nav-mobile');
 
 hamb.addEventListener('click', function () {
 	hamb.classList.toggle('active');
+	nav.classList.toggle('active');
 });
