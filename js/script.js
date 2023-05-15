1478cfdc84901e31dd74820d647465e6144ce9d8
ArtPlatosha <zipex98@gmail.com>
2023-05-15 10:16:46 +0300

    commit: 4th commit
    activating hamb (JS)

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// activate the hamb menu and mobile navigation
+const hamb = document.querySelector('.hamb');
+
+hamb.addEventListener('click', function () {
+	hamb.classList.toggle('active');
+});
